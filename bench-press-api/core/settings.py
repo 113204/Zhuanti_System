9c3d87023fdbcbc6022cf577906ae7e81bb81d77
Epoch <10756022@ntub.edu.tw>
2024-05-30 11:31:11 +0800

    edit: heroku

--- bench-press-api/core/settings.py
+++ bench-press-api/core/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$n+425&%ur1wv@l6%3ko^hf_n%8@1z@$kcze(&j7kpq1v6(mjt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # 在Heroku用的連線資訊
 # DEBUG = False
